907933baa931e69e8d0ada6e7e9e1c8256495805
Lucas Donghoon Lee <yanggun7201@gmail.com>
2018-10-17 12:09:16 +1300

    Create test code for getFilteredData

--- src/components/common/utils.test.js
+++ src/components/common/utils.test.js
@@ -1,3 +1,46 @@
-test("test", () => {
-    console.log("jest test");
+import { getSortedData, getFilteredData } from "./utils";
+
+const dave = {
+    name: "Dave",
+    email: "123dave@gmail.com",
+    phone: "123 456 7890"
+};
+
+const lucas = {
+    name: "Lucas",
+    email: "456lucas@gmail.com",
+    phone: "123 456 7890"
+};
+
+const john = {
+    name: "John",
+    email: "789john@gmail.com",
+    phone: "123 456 7890"
+};
+
+const initData = [dave, lucas, john];
+
+test("Filter correct data with filter value", () => {
+    const data = initData;
+    let filters = null;
+
+    filters = {
+        name: "d" // Dave
+    };
+    expect(getFilteredData(filters, data)).toEqual([dave]);
+
+    filters = {
+        name: "j" // John
+    };
+    expect(getFilteredData(filters, data)).toEqual([john]);
+
+    filters = {
+        name: "l" // Lucas
+    };
+    expect(getFilteredData(filters, data)).toEqual([lucas]);
+
+    filters = {
+        name: "a" // Dave, Lucas
+    };
+    expect(getFilteredData(filters, data)).toEqual([dave, lucas]);
 });
