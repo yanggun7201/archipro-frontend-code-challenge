f20a7cd2cceab0d52a5349d88fe63f0a1e0207ac
Lucas Donghoon Lee <yanggun7201@gmail.com>
2018-10-17 12:18:02 +1300

    Create test code for getSortedData

--- src/components/common/utils.test.js
+++ src/components/common/utils.test.js
@@ -44,3 +44,41 @@ test("Filter correct data with filter value", () => {
     };
     expect(getFilteredData(filters, data)).toEqual([dave, lucas]);
 });
+
+test("return empty array from getSortedData when data is empty", () => {
+    const data = [];
+    let sortColumn = "name";
+    let sortAsc = true;
+    expect(getSortedData(data, sortColumn, sortAsc)).toEqual([]);
+});
+
+test("return empty array from getSortedData when sortColumn is not available", () => {
+    const data = [];
+    let sortColumn = null;
+    let sortAsc = true;
+    expect(getSortedData(data, sortColumn, sortAsc)).toEqual([]);
+});
+
+test("basic sort test", () => {
+    const data = initData;
+    let sortColumn = "name";
+    let sortAsc = true;
+
+    //
+
+    sortColumn = "name";
+    sortAsc = true;
+    expect(getSortedData(data, sortColumn, sortAsc)).toEqual([dave, john, lucas]);
+
+    sortColumn = "name";
+    sortAsc = false;
+    expect(getSortedData(data, sortColumn, sortAsc)).toEqual([lucas, john, dave]);
+
+    sortColumn = "email";
+    sortAsc = true;
+    expect(getSortedData(data, sortColumn, sortAsc)).toEqual([dave, lucas, john]);
+
+    sortColumn = "email";
+    sortAsc = false;
+    expect(getSortedData(data, sortColumn, sortAsc)).toEqual([john, lucas, dave]);
+});
