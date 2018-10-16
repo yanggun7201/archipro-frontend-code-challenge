2388c417ca3b362350ac1ba02ff9d506ee14d1ae
Lucas Donghoon Lee <yanggun7201@gmail.com>
2018-10-17 10:28:33 +1300

    Start

--- src/App.js
+++ src/App.js
@@ -1,62 +1,59 @@
-import React, { Component } from 'react';
-import { Container, Row, Col, Table } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import { Container, Row, Col, Table } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
-import data from './api/data.json';
-import logo from './archipro_dev.webp';
+import "./App.css";
+import data from "./api/data.json";
+import logo from "./archipro_dev.webp";
 
 class App extends Component {
+    constructor() {
+        this.state = {
+            data
+        };
+    }
 
+    getTable() {
+        return (
+            <Table className="App-table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Contact Number</th>
+                    </tr>
+                </thead>
+                <tbody>{this.getRow()}</tbody>
+            </Table>
+        );
+    }
 
-  getTable() {
-    return (
-      <Table className="App-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Contact Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.getRow()}
-        </tbody>
-      </Table>
-    );
-  }
+    getRow() {
+        return data.map(({ _id, name, email, phone }) => (
+            <tr key={_id}>
+                <td>{name}</td>
+                <td>{email}</td>
+                <td>{phone}</td>
+            </tr>
+        ));
+    }
 
-  getRow() {
-    return data.map(
-      ({ _id, name, email, phone }) => (
-        <tr key={_id}>
-          <td>{name}</td>
-          <td>{email}</td>
-          <td>{phone}</td>
-        </tr>
-      )
-    )
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <main className="App-content">
-          <Container>
-            <Row>
-              <Col>
-              {this.getTable()}
-              </Col>
-            </Row>
-          </Container>
-
-        </main>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+                <main className="App-content">
+                    <Container>
+                        <Row>
+                            <Col>{this.getTable()}</Col>
+                        </Row>
+                    </Container>
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
